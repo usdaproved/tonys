95353626ce81e20a11e3d03b04c3bd09b480e25a
usdaproved <tbrock959@gmail.com>
2019-06-02 10:50:05 -0500

    Progress on order page.

--- order.php
+++ order.php
@@ -1,7 +1,16 @@
 <?php require 'includes/header.php' ?>
 
+<link rel="stylesheet" type="text/css" href="css/order.css">
+<script async src="js/order.js"></script>
 
+<div class="grid-container">
+    
+<div class="flex-container">
+    <div class="flex-item">Taco</div>
+    <div class="flex-item">Burrito</div>
+    <div class="flex-item">Nacho</div>
+</div>
 
-
+</div>
 
 <?php require 'includes/footer.php' ?>
