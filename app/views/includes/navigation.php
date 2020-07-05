df2892078e2c4966a497517cd75f558b62e0c765
usdaproved <me@trystanbrock.dev>
2020-07-04 20:55:15 -0500

    Mobile friendly header, revised header all around.

--- app/views/includes/navigation.php
+++ app/views/includes/navigation.php
@@ -1,3 +1,4 @@
+<?php // (C) Copyright 2020 by Trystan Brock All Rights Reserved. ?>
 <?php $displayUser = false; ?>
 <?php $loggedIn = false; ?>
 <?php $userType = 0; ?>
@@ -11,7 +12,7 @@
 
 <link href="<?= $this->getFile('css', __FILE__); ?>" rel="stylesheet">
 <header>
-    <nav class="menu">
+    <nav class="desktop-menu">
 	<ul>
 	    <?php if($userType >= EMPLOYEE) : ?>
 		<li class="dropdown-container">
@@ -33,10 +34,18 @@
 	</ul>
     </nav>
 
-    <h3 class="logo <?=($this instanceof HomeController) ? 'big' : ''?>">Tony's Taco House</h3>
+    <h3 class="restaurant-name">Tony's Taco House</h3>
+
+    <div class="mobile-hamburger">
+	<button type="button" class="mobile-hamburger-button" id="mobile-hamburger-button">
+	    <svg class="navigation-svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+			<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"/>
+		</svg>
+	</button>
+    </div>
     
     
-    <nav class="user-actions">
+    <nav class="desktop-user-actions">
 	<ul>
 	    <?php if(!$loggedIn): ?>
 		<li><a href="/register">Register</a></li>
@@ -56,7 +65,113 @@
 		</li>
 	    <?php endif; ?>
 	</ul>
-    </nav>
+	</nav>	
 </header>
+<div class="mobile-menu-container" id="mobile-menu-container">
+	<div class="mobile-menu">
+		<div class="space-y-6">
+			<div class="mobile-exit-button-container">
+				<button type="button" class="mobile-menu-exit-button" id="mobile-menu-exit-button">
+					<svg class="navigation-svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"/>
+					</svg>
+				</button>
+			</div>
+			<div class="mobile-user-container">
+				<nav class="mobile-menu-nav">
+					<a href="/" class="mobile-nav-link">
+						Home
+					</a>
+					<a href="/Order" class="mobile-nav-link">
+						Order
+					</a>
+				</nav>
+				<div class="space-y-6">
+					<div class="mobile-user-actions-container">
+						<nav class="mobile-user-actions">
+						<?php if($displayUser): ?>
+							<a href="/User/orders" class="user-link">Order History</a>
+							<a href="/User/info"  class="user-link">Info</a>
+							<a href="/User/address"  class="user-link">Address</a>
+						<?php endif; ?>
+						</nav>
+						<nav class="mobile-user-actions">
+						<?php if($userType >= EMPLOYEE) : ?>
+							<a href="#" class="mobile-dashboard-title">Dashboard</a>
+							<a href="/Dashboard/orders/active"  class="user-link">Active Orders</a>
+							<a href="/Dashboard/orders/search"  class="user-link">Search Orders</a>
+							<a href="/Dashboard/customers/search"  class="user-link">Search Customers</a>
+							<?php if($userType == ADMIN): ?>
+								<a href="/Dashboard/menu"  class="user-link">Menu</a>
+								<a href="/Dashboard/employees"  class="user-link">Employees</a>
+								<a href="/Dashboard/settings"  class="user-link">Settings</a>
+							<?php endif; ?>
+						<?php endif; ?>
+						</nav>
+					</div>
+					<?php if(!$loggedIn): ?>
+					<div class="space-y-6">
+						<span class="register-container">
+							<a href="/register" class="register-button">
+								Register
+							</a>
+						</span>
+						<p class="login-container">
+							Existing customer?
+							<a href="/login" class="login-button">
+								Sign in
+							</a>
+						</p>
+					</div>
+					<?php endif; ?>
+					<?php if($loggedIn): ?>
+						<div class="logout-container">
+						<a href="/logout" class="logout-button">
+							Log out
+						</a>
+						</div>
+					<?php endif; ?>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<script>
+	"use strict";
+
+	const mobileHamburgerButton = document.querySelector('#mobile-hamburger-button');
+	const mobileMenuExitButton = document.querySelector('#mobile-menu-exit-button');
+	const mobileMenuElement = document.querySelector('#mobile-menu-container');
+
+	let mobileMenuActive = false;
+
+	const toggleMobileMenu = () => {
+		if(mobileMenuElement.style.display === 'block'){
+			mobileMenuElement.style.display = 'none';
+			mobileMenuActive = false;
+		} else {
+			mobileMenuElement.style.display = 'block';
+			mobileMenuActive = true;
+		}
+	};
+
+	mobileHamburgerButton.addEventListener('click', (e) => {
+		toggleMobileMenu();
+	});
+
+	mobileMenuExitButton.addEventListener('click', (e) => {
+		toggleMobileMenu();
+	});
+
+	window.addEventListener('resize', (e) => {
+		if(window.innerWidth > 768){
+			mobileMenuElement.style.display = 'none';
+		} else if (window.innerWidth < 768) {
+			if(mobileMenuActive){
+				mobileMenuElement.style.display = 'block';
+			}
+		}
+	});
+</script>
 
     
